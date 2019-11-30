9320a6381f5af27a34bd6776f1da56671c752da8
Jake <gosnelljacob@gmail.com>
2019-11-29 20:02:00 -0800

    refactored a class component to use hooks to increase code readability and optimization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Toggle from './Toggle';
 
 const App = () => {
 
@@ -8,6 +9,7 @@ const [name, setName] = useState('');
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <Toggle />
       <h3>{name}</h3>
       <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
     </div>
---
+++ src/Toggle.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const Toggle = () => {
+  const [ isToggled, setToggle ] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setToggle(!isToggled)}>Click Me!</button>
+      {isToggled && <h2>Hello</h2>}
+    </div>
+  );
+}
+
+export default Toggle;
+
+
+// import React, { Component } from "react";
+
+// export default class Refactor extends Component {
+//   state = {
+//     isToggled: false
+//   };
+
+//   toggle = () => {
+//     this.setState(state => {
+//       return { isToggled: !state.isToggled };
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.toggle}>Toggle</button>
+//         {this.state.isToggled && <h2>Hello!</h2>}
+//       </div>
+//     );
+//   }
+// }
+
+// we have a state initialized, a toggle function that 
+// takes the current state and sets it to the reversed 
+// version of the state
