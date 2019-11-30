231e9566b9965221cd8525021504bd7248a9c89f
Jake <gosnelljacob@gmail.com>
2019-11-29 20:38:37 -0800

    created a form in app.js that uses hooks and also submits the form to console log and simulate a server side function in sending an email to the email that was entered.

--- src/App.js
+++ src/App.js
@@ -10,10 +10,25 @@ const [name, setName] = useState('');
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
       <Toggle />
-      <h3>{name}</h3>
-      <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+      <form onSubmit={e => {
+        e.preventDefault();
+        formSubmit(name, setName);
+      }}>
+        <input 
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name} 
+        />
+        <button>Submit</button>
+      </form>
+      
     </div>
   );
 };
+
+const formSubmit = (value, setValue) => {
+  console.log('email sent to ' + value + '!');
+  setValue('');
+}
 
 export default App;
